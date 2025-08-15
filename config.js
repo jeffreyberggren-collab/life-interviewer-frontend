186f1ddee551f19fc50c6b6c20cb6e6b4e846020
jeffreyberggren-collab <jeffrey.berggren@gmail.com>
2025-08-15 19:46:46 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // IMPORTANT: Set this to your Render server origin.
 window.APP_CONFIG = {
-  SERVER_URL: "" // e.g., https://life-interviewer-server.onrender.com
+  SERVER_URL: "https://life-interviewer-server.onrender.com"
 };
